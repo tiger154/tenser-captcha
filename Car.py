c71a841136dff5e37038b6ea20ecfed21022f885
jeonhwanwon <jeonhwan.won@cj.net>
2018-11-27 10:20:19 +0900

    print String format error fix

--- Car.py
+++ Car.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     while True:
         action = input("What should I do? [A]ccelerate, [B]rake, "
                        "show [O]dometer, or show average [S]peed?").upper()
-        if action not in  "ABOS" or len(action) != 1:
+        if action not in "ABOS" or len(action) != 1:
             print("I don't know how to do that")
             continue
         if action == "A":
@@ -38,6 +38,6 @@ if __name__ == '__main__':
         elif action == "O":
             print("The car has driven {} kilometers".format(my_car.odometer))
         elif action == "S":
-            print("The car's average speed was {} kph").format(my_car.average_speed())
+            print("The car's average speed was {} kph".format(my_car.average_speed()))
         my_car.step()
         my_car.say_state()
