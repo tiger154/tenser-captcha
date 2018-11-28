bf9597b5776a37f175921d00bc69ea0d1db5be07
jeonhwanwon <jeonhwan.won@cj.net>
2018-11-28 17:56:00 +0900

    added one more site

--- SimpleLoginDaemon.py
+++ SimpleLoginDaemon.py
@@ -35,18 +35,28 @@ FB_XPATH_LOGIN_BTN = '//*[@id="login_form"]//*[@type="submit"]'
 FB_USER_ID = os.getenv("FB_USER_ID")        # NAVER_PASSWORD TARGETPICK_USER_ID
 FB_PASSWORD = os.getenv("FB_PASSWORD")      # NAVER_PASSWORD TARGETPICK_USER_ID
 
-LOGIN_TYPE = "tp"
+CJ_LOGIN_URL = os.getenv("CJ_LOGIN_URL")
+CJ_MAIL_URL = os.getenv("CJ_MAIL_URL")
+CJ_ID_TAG_NAME = "txtID"
+CJ_PW_TAG_NAME = "txtPWD"
+CJ_XPATH_LOGIN_BTN = '//*[@id="frm_login"]//*[@class="btn_login"]'
+CJ_USER_ID = os.getenv("CJ_USER_ID")        # CJ_PASSWORD TARGETPICK_USER_ID
+CJ_PASSWORD = os.getenv("CJ_PASSWORD")      # CJ_PASSWORD TARGETPICK_USER_ID
+
+
+LOGIN_TYPE = "fb"
 # ============
 
 
 class LoginData:
-    def __init__(self, login_url, id_tag_name, pw_tag_name, xpath_login_btn, user_id, password):
+    def __init__(self, login_url, id_tag_name, pw_tag_name, xpath_login_btn, user_id, password, lazy_input_type=True):
         self.login_url = login_url
         self.id_tag_name = id_tag_name
         self.pw_tag_name = pw_tag_name
         self.xpath_login_btn = xpath_login_btn
         self.user_id = user_id
         self.password = password
+        self.lazy_input_type = lazy_input_type
 
 
 class SimpleLoginDaemon:
@@ -90,6 +100,8 @@ class SimpleLoginDaemon:
                 login_data = LoginData(FB_LOGIN_URL, FB_ID_TAG_NAME, FB_PW_TAG_NAME, FB_XPATH_LOGIN_BTN, FB_USER_ID, FB_PASSWORD)
             elif type == "naver":
                 login_data = LoginData(NAVER_LOGIN_URL, NAVER_ID_TAG_NAME, NAVER_PW_TAG_NAME, NAVER_XPATH_LOGIN_BTN, NAVER_USER_ID, NAVER_PASSWORD)
+            elif type == "cj":
+                login_data = LoginData(CJ_LOGIN_URL, CJ_ID_TAG_NAME, CJ_PW_TAG_NAME, CJ_XPATH_LOGIN_BTN, CJ_USER_ID, CJ_PASSWORD, False)
             else:
                 login_data = LoginData(TP_LOGIN_URL, TP_ID_TAG_NAME, TP_PW_TAG_NAME, TP_XPATH_LOGIN_BTN, TP_USER_ID, TP_PASSWORD)
 
@@ -99,6 +111,7 @@ class SimpleLoginDaemon:
             xpath_login_btn = login_data.xpath_login_btn
             user_id = login_data.user_id
             password = login_data.password
+            lazy_input_type = login_data.lazy_input_type
 
             # 1. LAND TO LOGIN PAGE
             self.driver.get(login_uri)
@@ -106,21 +119,43 @@ class SimpleLoginDaemon:
 
             # 2. TYPE ID/PW
             el_id = self.driver.find_element_by_name(tag_id)
-            for character in user_id:
-                el_id.send_keys(character)
-                time.sleep(randrange(1, 5)/10)
-            #self.driver.find_element_by_name('id').send_keys(userId)
+            if lazy_input_type:
+                key = 0
+                for character in user_id:
+                    el_id.send_keys(character)
+                    if key < 4:
+                        time.sleep(randrange(1, 5)/10)
+                    else:
+                        time.sleep(randrange(1, 10)/100)
+                    key += 1
+            else:
+                el_id.send_keys(user_id)
             time.sleep(randrange(2, 5))
 
             el_pw = self.driver.find_element_by_name(tag_pw)
-            for character in password:
-                el_pw.send_keys(character)
-                time.sleep(randrange(2, 6)/10)
-            #self.driver.find_element_by_name("pw").send_keys(password)
+            if lazy_input_type:
+                for character in password:
+                    el_pw.send_keys(character)
+                    time.sleep(randrange(2, 6)/10)
+            else:
+                el_pw.send_keys(password)
             time.sleep(randrange(2, 4))
 
             # 3. TRY TO LOGIN
             self.driver.find_element_by_xpath(xpath_login_btn).click()
+            self.driver.implicitly_wait(3)
+
+            # 4. After login
+            if type == "cj":
+                self.driver.get(CJ_MAIL_URL)
+                time.sleep(2)
+                # crawl email title and print out..
+                page = self.driver.page_source
+                soup = BeautifulSoup(page)
+                titles = soup.select(".tdSubject")
+                for title in titles:
+                    print(">> ## 자동 메일 스크랩 제목입니다: " + title.text)
+
 
             try:
                 self.driver.find_element_by_class_name('link_login_help')
